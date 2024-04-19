f6ca3e477a807e4d5359d9f427aefda89e8fdfae
Bismay <bismaysarangi@gmail.com>
2024-04-19 19:50:57 +0530

    Day 2

---
+++ Array-Day1/array-creation.py
@@ -0,0 +1,12 @@
+#import array 
+
+# my_array = array.array('i', [1, 2, 3, 4])
+
+#print(my_array)
+
+import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5], dtype=int)
+
+arr = np.array([1, 2, 3])
+print(arr)
---
+++ Array-Day1/instertion-in-array.py
@@ -0,0 +1,6 @@
+import array 
+
+my_array = array.array('i', [1, 2, 3, 4])
+my_array.insert(0, 7)
+
+print(my_array)
