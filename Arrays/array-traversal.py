3104fba90e8bb1154746c3c236b19dea2efe2c75
Bismay <bismaysarangi@gmail.com>
2024-04-22 23:31:48 +0530

    traversal

---
+++ Arrays/array-traversal.py
@@ -0,0 +1,17 @@
+# my_array = ['1', '2', '3']
+
+def traversal(my_array):
+    for i in my_array:
+        print(i, end = " ")
+    print()
+
+def access_element(array, index):
+    if index > len(array):
+        print("There is no element in this index")
+    else:
+        print(array[index])
+
+
+traversal(['1', '2', '3'])
+
+access_element(['1', '2', '3'], 1)
