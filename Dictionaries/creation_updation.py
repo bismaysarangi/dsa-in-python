089cc918f3c4f9ccb919187a022346589b4269d0
Bismay <bismaysarangi@gmail.com>
2024-05-17 22:42:50 +0530

    Dict intro

---
+++ Dictionaries/creation_updation.py
@@ -0,0 +1,19 @@
+my_dict = dict()
+print(my_dict) 
+
+# First Method
+# translate = dict(one = 'uno', two = 'dos', three = 'tres')
+# print(translate)
+
+# Second Method
+trans = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+print(trans)
+
+# Updation
+trans['three'] = 'teen'
+print(trans)
+
+# Addition
+trans['four'] = 'chaar'
+print(trans)
+
