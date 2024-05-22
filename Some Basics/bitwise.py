2a9dcca78ca9b229bef01082d86ab3efff7aab30
Bismay <bismaysarangi@gmail.com>
2024-05-22 18:15:41 +0530

    Bitwise Operators Done.

---
+++ Some Basics/bitwise.py
@@ -0,0 +1,17 @@
+print(~12)
+
+print(12 & 13)
+
+print(12 | 13)
+
+print(25 & 30)
+
+print(10 >> 2) #Right Shift 2 bits(0s)
+
+print(10 << 2)#Left Shift 2 bits(0s)
+
+res = 0
+for i in range(32):
+    if (10 >> i) & 1:
+        res += 1
+print(res)
