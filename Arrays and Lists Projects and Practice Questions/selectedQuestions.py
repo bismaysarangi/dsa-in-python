4f8f2780da688ec33e5d93888f2d49ff78658cf0
Bismay <bismaysarangi@gmail.com>
2025-05-03 20:28:56 +0530

    Add comments for spiral matrix problem to clarify input and output

--- Arrays and Lists Projects and Practice Questions/selectedQuestions.py
+++ Arrays and Lists Projects and Practice Questions/selectedQuestions.py
@@ -99,6 +99,8 @@ def findNumbers(self, nums: List[int]) -> int:
         return count
 
 # Problem: Spiral Matrix
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [1,2,3,6,9,8,7,4,5]
 def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
         res = []
         while matrix:
