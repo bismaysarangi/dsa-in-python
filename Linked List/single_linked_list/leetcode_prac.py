79da4917b0fba86066c76329e183c763a275fd85
Bismay <bismaysarangi@gmail.com>
2025-06-13 21:21:59 +0530

    Add mergeTwoLists function to merge two sorted linked lists

--- Linked List/single_linked_list/leetcode_prac.py
+++ Linked List/single_linked_list/leetcode_prac.py
@@ -25,3 +25,20 @@ def isPlaindroome(self, head: optional[Node]) -> bool:
         left = left.next
         right = right.next
     return True 
+
+#Merge two sorted lists
+def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    cur = dummy = ListNode()
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            cur.next = list1
+            list1, cur = list1.next, list1
+        else:
+            cur.next = list2
+            list2, cur = list2.next, list2
+        
+    if list1 or list2:
+        cur.next = list1 if list1 else list2
+    
+    return dummy.next
