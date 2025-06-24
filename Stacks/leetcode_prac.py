2db707e4aa28e69ce9640433d5de6e83a7ab381a
Bismay <bismaysarangi@gmail.com>
2025-06-24 21:55:13 +0530

    Add MinStack class implementation with push, pop, top, and getMin methods

--- Stacks/leetcode_prac.py
+++ Stacks/leetcode_prac.py
@@ -11,3 +11,27 @@ def isValid(self, s: str) -> bool:
                 return False
     
     return not stack
+
+# Problem: Min Stack
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            current_min = val
+        else:
+            current_min = min(val, self.stack[-1][1])
+        self.stack.append((val, current_min))
+        
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1][0] if self.stack else None
+
+    def getMin(self) -> int:
+        return self.stack[-1][1] if self.stack else None
+        
