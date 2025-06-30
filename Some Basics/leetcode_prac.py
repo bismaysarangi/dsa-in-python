9f13e52daaf46f3a0da7735cb0adac5c96642ff0
Bismay <bismaysarangi@gmail.com>
2025-06-30 23:11:43 +0530

    Refactor isPalindrome function and add searchRange function to find first and last position of an element in a sorted array

--- Some Basics/leetcode_prac.py
+++ Some Basics/leetcode_prac.py
@@ -10,3 +10,15 @@ def isPalindrome(self, s: str) -> bool:
         if new[::-1] == new:
             return True
         return False
+
+# Find First and Last Position of Element in Sorted Array
+def searchRange(self, nums: List[int], target: int) -> List[int]:
+        indexes = []
+        if target not in nums:
+            return [-1, -1]
+
+        for i in range(len(nums)):
+            if nums[i] == target:
+                indexes.append(i)
+        
+        return [indexes[0], indexes[-1]]
