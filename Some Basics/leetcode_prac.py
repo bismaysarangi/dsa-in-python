bcef86960ce293f5fd47e3d8b804e639cf968cf5
Bismay <bismaysarangi@gmail.com>
2025-07-01 22:01:29 +0530

    Add possibleStringCount function to count distinct characters in a string

--- Some Basics/leetcode_prac.py
+++ Some Basics/leetcode_prac.py
@@ -22,3 +22,13 @@ def searchRange(self, nums: List[int], target: int) -> List[int]:
                 indexes.append(i)
         
         return [indexes[0], indexes[-1]]
+
+# Problem: Possible String count
+def possibleStringCount(self, word: str) -> int:
+        n = len(word)
+        count = n
+        for i in range(1, n):
+            if word[i] != word[i - 1]:
+                count -= 1
+        
+        return count
