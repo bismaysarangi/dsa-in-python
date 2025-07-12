8ae84e2c61f3f0c28aaf66a947b976ac86e28495
Bismay <bismaysarangi@gmail.com>
2025-07-12 19:43:18 +0530

    Add longestMountain function to find the longest mountain in an array

--- Arrays/Arrays and Lists Projects and Practice Questions/selectedQuestions.py
+++ Arrays/Arrays and Lists Projects and Practice Questions/selectedQuestions.py
@@ -153,3 +153,26 @@ def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
             return float(nums[n // 2]) 
         else:
             return (nums[n // 2 - 1] + nums[n // 2]) / 2
+        
+# Problem: Longest Mountain in an Array
+def longestMountain(self, arr: List[int]) -> int:
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+        res = 0
+        n = len(arr)
+
+        for i in range(1, n - 1):
+            if arr[i - 1] < arr[i] > arr[i + 1]:
+                l = i - 1
+                r = i + 1
+
+                while l > 0 and arr[l] > arr[l - 1]:
+                    l -= 1
+
+                while r < n - 1 and arr[r] > arr[r + 1]:
+                    r += 1
+                    i = r
+
+                res = max(res, r - l + 1)
+
+        return res
