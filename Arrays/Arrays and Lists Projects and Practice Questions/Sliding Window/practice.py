d07cd723a282004723bc74f5ae290a015a6fbfa5
Bismay <bismaysarangi@gmail.com>
2025-07-13 22:40:22 +0530

    Add minimumAbsDifference function to calculate minimum absolute difference in an array

--- Arrays/Arrays and Lists Projects and Practice Questions/Sliding Window/practice.py
+++ Arrays/Arrays and Lists Projects and Practice Questions/Sliding Window/practice.py
@@ -7,3 +7,17 @@ def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
             return True
         index_map[num] = i
     return False
+
+# Problem: Minimum Absolute Difference
+def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+    arr.sort()
+    min_diff = float('inf')
+
+    for i in rannge(1, len(arr)):
+        min_diff = min(min_diff, arr[i] - arr[i - 1])
+    
+    result = []
+    for i in range(1, len(arr)):
+        if arr[i] - arr[i - 1] == min_diff:
+            result.append([arr[i - 1], arr[i]])
+    return result
