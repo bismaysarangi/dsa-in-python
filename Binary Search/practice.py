98a5c8cff6db55d28fc1eafde8fa400d900cc0c8
Bismay <bismaysarangi@gmail.com>
2025-07-17 14:40:51 +0530

    Add firstBadVersion function to identify the first bad version in a sequence

--- Binary Search/practice.py
+++ Binary Search/practice.py
@@ -31,3 +31,17 @@ def searchInsert(self, nums: List[int], target: int) -> int:
         
         return left
 
+# Problem: First Bad Version
+def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
+
