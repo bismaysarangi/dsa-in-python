2336cbf130540c16065af4a83593de407ed0e24e
Bismay <bismaysarangi@gmail.com>
2025-07-19 17:55:13 +0530

    Add isPerfectSquare function to determine if a number is a perfect square

--- Binary Search/practice.py
+++ Binary Search/practice.py
@@ -45,3 +45,19 @@ def firstBadVersion(self, n: int) -> int:
         
         return left
 
+# Problem: Valid Perfect Square
+def isPerfectSquare(self, num: int) -> bool:
+     low = 0
+     high = num
+
+     while low <= high:
+          mid = (low + high) // 2
+          square = mid * mid
+          if square == num:
+               return True
+          elif square < num:
+               low = mid + 1
+          else:
+               high = mid - 1
+               
+     return False
