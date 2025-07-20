aebbaa2a7b96ece0e8a986193b98ca1ddc4a9c26
Bismay <bismaysarangi@gmail.com>
2025-07-20 21:48:02 +0530

    Add findClosestNumber function to determine the closest number to zero in a list

--- Arrays/Arrays and Lists Projects and Practice Questions/selectedQuestions.py
+++ Arrays/Arrays and Lists Projects and Practice Questions/selectedQuestions.py
@@ -13,6 +13,18 @@ def missingNumber(self, nums: List[int]) -> int:
         s = sum(nums)
         return tot_sum - s
 
+# Problem: Find closest number to zero
+def findClosestNumber(self, nums: List[int]) -> int:
+        closest = nums[0]
+
+        for num in nums:
+            if abs(num) < abs(closest):
+                closest = num
+            elif abs(num) == abs(closest):
+                closest = max(num, closest)
+
+        return closest 
+
 # Problem: Find all missing numbers
 def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         # Time Complexity: O(n)
