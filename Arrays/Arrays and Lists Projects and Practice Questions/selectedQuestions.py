8f80a7f0e4d96f220d68457b5437a4eb57d41e54
Bismay <bismaysarangi@gmail.com>
2025-07-21 20:56:18 +0530

    Add rotate function to rotate a matrix 90 degrees clockwise

--- Arrays/Arrays and Lists Projects and Practice Questions/selectedQuestions.py
+++ Arrays/Arrays and Lists Projects and Practice Questions/selectedQuestions.py
@@ -214,3 +214,12 @@ def longestMountain(self, arr: List[int]) -> int:
                 res = max(res, r - l + 1)
 
         return res
+
+# Problem: Rotate Image
+def rotate(self, matrix: List[List[int]]) -> None:
+     matrix.reverse()
+     n = len(matrix)
+
+     for i in range(n):
+          for j in range(i, n):
+               matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
