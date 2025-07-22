fe670415e257f3d2fb634b803387b55c7a71ad04
Bismay <bismaysarangi@gmail.com>
2025-07-22 21:16:40 +0530

    Add searchMatrix function to search for a target in a 2D matrix

--- Binary Search/practice.py
+++ Binary Search/practice.py
@@ -61,3 +61,25 @@ def isPerfectSquare(self, num: int) -> bool:
                high = mid - 1
                
      return False
+
+# Problem: Search in a 2D Matrix
+def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+     m = len(matrix)
+     n = len(matrix[0])
+     t = m * n
+     l = 0
+     r = t - 1
+
+     while l <= r:
+          mid = (l + r) // 2
+          i = mid // n
+          j = mid % n
+
+          if matrix[i][j] == target:
+                return True
+          elif matrix[i][j] < target:
+                l = mid + 1
+          else:
+                r = mid - 1
+
+     return False
