8ab8e00ebdb59ada26c55c11424d8340bac861a9
Bismay <bismaysarangi@gmail.com>
2025-07-24 22:46:02 +0530

    Add removeElements function to remove specified elements from a linked list

--- Linked List/single_linked_list/leetcode_prac.py
+++ Linked List/single_linked_list/leetcode_prac.py
@@ -83,3 +83,17 @@ def getDecimalValue(self, head: Optional[ListNode]) -> int:
             curr = curr.next
         
         return ans
+
+# Problem: Remove Linked List Elements
+def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+     while head and head.val == val:
+          head = head.next
+     temp = head
+
+     while temp and temp.next:
+          if temp.next.val == val:
+               temp.next = temp.next.next
+          else:
+               temp = temp.next
+     return head
+
