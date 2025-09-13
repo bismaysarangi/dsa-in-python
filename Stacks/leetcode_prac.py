04134e6fc084012f71de7325ff4273ec65eb2a4c
Bismay <bismaysarangi@gmail.com>
2025-09-13 21:52:04 +0530

    Add longestCommonPrefix function to find the longest common prefix among strings

--- Stacks/leetcode_prac.py
+++ Stacks/leetcode_prac.py
@@ -122,3 +122,17 @@ def isIsomorphic(s: str, t: str) -> bool:
         mapTS[charT] = charS
 
     return True
+
+# Problem: Longest Common Prefix
+def longestCommonPrefix(strs: List[str]) -> str:
+    if not strs:
+        return ""
+    
+    for i in range(len(strs[0])):
+        char = strs[0][i]
+
+        for word in strs[1:]:
+            if i == len(word) or word[i] != char:
+                return strs[0][:i]
+    
+    return strs[0]
